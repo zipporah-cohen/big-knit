fa063ee3491e3230bec710b07422ba70de08b74b
zipporah-cohen <65784162+zipporah-cohen@users.noreply.github.com>
2022-11-06 14:40:46 -0500

    toggle cnc and corrected direction

--- bigKnit.ino
+++ bigKnit.ino
@@ -5,42 +5,54 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 /*
- * In this implementation, AccelStepper and limit switches together is a bit redundant.
- * Using both here keeps our options open, but if we use AccelStepper, we could conceivably not need limit swtiches.
- * We only need to do the math to determine our desired position based on distance.
+ * Next to fix: currently relies on the limit switch being quickly pressed and released. Start just reading the limit switch's analog directly.
+ * With current logic, will run forever until limit switch is pressed. Change logic to take specified steps and stop at end or at limit switch. 
  * 
  */
 
 //constants
-const int STEPPER_PORT = 2; //M3 and M4 of MotorShield
+const int AF_STEPPER_PORT = 2; //M3 and M4 of MotorShield
 const int STEPS_PER_REV = 200; //smaller stepper
 const int INCH_PER_REV = 1; //place holder val
-const int LIMIT_PIN1 = 5; //NC pin
-const int LIMIT_PIN2 = 6; //NC pin
+const int AF_LIMIT_PIN1 = A0; //NC pin
+const int AF_LIMIT_PIN2 = A1; //NC pin
 
-int desiredPos = 1000;
+const bool USE_CNC = false;
+
+bool manualStop;
+int distToMove;
+int stepsToTake;
+int stepSpeed;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor *myStepper = AFMS.getStepper(STEPS_PER_REV, STEPPER_PORT);
-ezButton limitSwitch1(LIMIT_PIN1); 
-ezButton limitSwitch2(LIMIT_PIN2); 
+Adafruit_StepperMotor *myStepper = AFMS.getStepper(STEPS_PER_REV, AF_STEPPER_PORT);
 
 void forwardStep() {
   myStepper->onestep(FORWARD, SINGLE);
 }
-
 void backwardStep() {
   myStepper->onestep(BACKWARD, SINGLE);
 }
 
 AccelStepper stepper(forwardStep, backwardStep);
 
+ezButton limitSwitch1(AF_LIMIT_PIN1); 
+ezButton limitSwitch2(AF_LIMIT_PIN2); 
+
 void setup() {
-  AFMS.begin();
-  Serial.begin(9600);
+  if(!USE_CNC){
+    AFMS.begin();
+  }
   
-  stepper.setSpeed(50);
-  stepper.moveTo(desiredPos);
+  Serial.begin(9600);
+
+  manualStop = false;
+  distToMove = 1000;//use later with math
+  stepsToTake = 50;
+  stepSpeed = 50; 
+
+  stepper.setCurrentPosition(stepper.currentPosition()); //set current pos to be the new 0
+  stepper.setSpeed(stepSpeed);
 
   limitSwitch1.setDebounceTime(50);
   limitSwitch2.setDebounceTime(50);
@@ -59,29 +71,45 @@ void loop() {
       Serial.println("knitting");
     }
     else if(str == "stop"){
-      Serial.println("stopped");
+      manualStop = true;
+      stepper.stop();
     }
   }
 
   limitSwitch1.loop();
   limitSwitch2.loop();
 
-  //while neither is pressed, move the motor (could use isReleased() instead of LOW)
-  while(limitSwitch1.getState() == LOW && limitSwitch2.getState() == LOW){
+  if(!manualStop){
+    if(USE_CNC){
+      cncLoop();
+    }
+    else{
+      afmsLoop();
+    }
+  }
+
+}
+
+void afmsLoop() {
+  
+  //while neither is pressed, move the motor
+  if(!(limitSwitch1.isPressed() || limitSwitch2.isPressed())){
+    Serial.println("Neither is pressed");
+    stepper.move(stepsToTake);
     stepper.run();
   }
 
   //when one is pressed, stop motor, change direction/desired position
   if(limitSwitch1.isPressed() ^ limitSwitch2.isPressed()){
     Serial.println("Swtich is pressed");
-    desiredPos = desiredPos*(-1);
-    stepper.moveTo(desiredPos);
+    stepper.stop();
+    stepsToTake = -stepsToTake;
+    //stepper.move(stepsToTake);
   }
+}
 
-  //Add check to make sure the switch is released after change
-  if(stepper.run() && (limitSwitch1.isPressed() || limitSwitch2.isPressed())){
-    Serial.println("Stepper is running but switch is pressed");
-  }
+void cncLoop()  {
+  //insert cnc code here
 }
 
 void homing(){}

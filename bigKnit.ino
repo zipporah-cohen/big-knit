a3b250cff44eec618e6cd06e20e3ab3f071165a5
zipporah-cohen <65784162+zipporah-cohen@users.noreply.github.com>
2022-11-09 14:47:35 -0500

    delete ezButton and change direction logic

--- bigKnit.ino
+++ bigKnit.ino
@@ -5,8 +5,8 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 /*
- * Next to fix: currently relies on the limit switch being quickly pressed and released. Start just reading the limit switch's analog directly.
- * With current logic, will run forever until limit switch is pressed. Change logic to take specified steps and stop at end or at limit switch. 
+ * Next to fix: it wil switch which limit to hit multiple times instead of once. 
+ * Try: putting it all in the main loop insted of getting called. Or, changing logic of if/elses to somehow account for the outlier.
  * 
  */
 
@@ -14,8 +14,8 @@
 const int AF_STEPPER_PORT = 2; //M3 and M4 of MotorShield
 const int STEPS_PER_REV = 200; //smaller stepper
 const int INCH_PER_REV = 1; //place holder val
-const int AF_LIMIT_PIN1 = A0; //NC pin
-const int AF_LIMIT_PIN2 = A1; //NC pin
+const int LIM_L = A0; //NC
+const int LIM_R = A1; //NC
 
 const bool USE_CNC = false;
 
@@ -23,6 +23,7 @@ bool manualStop;
 int distToMove;
 int stepsToTake;
 int stepSpeed;
+int nextLim;
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *myStepper = AFMS.getStepper(STEPS_PER_REV, AF_STEPPER_PORT);
@@ -36,26 +37,23 @@ void backwardStep() {
 
 AccelStepper stepper(forwardStep, backwardStep);
 
-ezButton limitSwitch1(AF_LIMIT_PIN1); 
-ezButton limitSwitch2(AF_LIMIT_PIN2); 
-
 void setup() {
   if(!USE_CNC){
     AFMS.begin();
   }
   
   Serial.begin(9600);
-
+  pinMode(LIM_L, INPUT);
+  pinMode(LIM_R, INPUT);
+  
   manualStop = false;
   distToMove = 1000;//use later with math
-  stepsToTake = 50;
+  stepsToTake = 100;
   stepSpeed = 50; 
+  nextLim = LIM_R; //which limit switch will get hit next
 
   stepper.setCurrentPosition(stepper.currentPosition()); //set current pos to be the new 0
   stepper.setSpeed(stepSpeed);
-
-  limitSwitch1.setDebounceTime(50);
-  limitSwitch2.setDebounceTime(50);
 }
 
 void loop() {
@@ -76,9 +74,6 @@ void loop() {
     }
   }
 
-  limitSwitch1.loop();
-  limitSwitch2.loop();
-
   if(!manualStop){
     if(USE_CNC){
       cncLoop();
@@ -92,19 +87,35 @@ void loop() {
 
 void afmsLoop() {
   
-  //while neither is pressed, move the motor
-  if(!(limitSwitch1.isPressed() || limitSwitch2.isPressed())){
-    Serial.println("Neither is pressed");
-    stepper.move(stepsToTake);
+  //if the limit to hit is unpressed and the stepper is moving, continue
+  if(analogRead(nextLim) == LOW && stepper.run()){
     stepper.run();
   }
-
-  //when one is pressed, stop motor, change direction/desired position
-  if(limitSwitch1.isPressed() ^ limitSwitch2.isPressed()){
-    Serial.println("Swtich is pressed");
+  //if the limit switch has been hit and the stepper is still running
+  else if(analogRead(nextLim) == HIGH && stepper.run()){
+    //stop the stepper
     stepper.stop();
+    //wait for stepper to stop moving
+    while(stepper.run()){}
+  }
+  //if the stepper has stopped regardless of limit switch
+  //(analogRead(AF_LIMIT_PIN1) == HIGH || analogRead(AF_LIMIT_PIN1) == LOW)
+  else{
+    Serial.println("stepper no longer running");
+
+    //switch limit to hit
+    if(nextLim == LIM_L){
+      Serial.println("Switching to hit limit R (A1)");
+      nextLim = LIM_R;
+    }
+    else{
+      Serial.println("Switching to hit limit L (A0)");
+      nextLim = LIM_L;
+    }
+    
     stepsToTake = -stepsToTake;
-    //stepper.move(stepsToTake);
+    stepper.move(stepsToTake);
+    stepper.run();
   }
 }
 
